55080085769ab4f60a6b8ab72b080bc3b349a76c
amaniilm <239107135+amaniilm@users.noreply.github.com>
2026-07-06 12:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,21 @@ parser=StrOutputParser()
 url=st.text_input('enter your url')
 question=st.text_input("ask your question")
 if st.button("get result"):
-    loader=YoutubeLoader.from_youtube_url(url,language=['en','hi'],add_video_info=False)
-    docs=loader.load()
-    prompt=PromptTemplate(
-    template="""you are the AI assistant 
-        use only the transcript to answer the following {question},from{topic}
-        If the transcript does not contain the answer, say:
-        "I couldn't find that information in the video."  """,
-        input_variables=['question','topic']
-    )
-    chain=prompt|model|parser
-    result=chain.invoke({'question':question,'topic':docs[0].page_content})
-    st.success(result)
+    try:
+        loader=YoutubeLoader.from_youtube_url(url,language=['en','hi'],add_video_info=False)
+        docs=loader.load()
+        prompt=PromptTemplate(
+        template="""you are the AI assistant 
+            use only the transcript to answer the following {question},from{topic}
+            If the transcript does not contain the answer, say:
+            "I couldn't find that information in the video."  """,
+            input_variables=['question','topic']
+        )
+        chain=prompt|model|parser
+        result=chain.invoke({'question':question,'topic':docs[0].page_content})
+        st.success(result)
+    except Exception as e:
+        st.error(
+            "Unable to fetch the transcript. The video may not have captions or YouTube has blocked the request. Please try another video."
+        )
+        st.stop()
