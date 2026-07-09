2a74656b1cd9dbf108bf80dce1355863a5827434
Aman Yadav <yadavlakshika96@gmail.com>
2026-07-09 11:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import YoutubeLoader
-from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import  streamlit as st
@@ -10,11 +9,10 @@ st.set_page_config(
     page_icon="🎥"
 )
 st .title("🎥 YouTube video AI Chatbot")
-if "GOOGLE_API_KEY" in st.secrets:
-    os.environ["GOOGLE_API_KEY"]=st.secrets['GOOGLE_API_KEY']
-else:
-    load_dotenv()
-model=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+if "GOOGLE_API_KEY" not in st.secrets:
+    st.error("google api key not found in streamlit")
+    st.stop()
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash",google_api_key=st.secrets["GOOGLE_API_KEY"])
 parser=StrOutputParser()
 url=st.text_input('enter your url')
 question=st.text_input("ask your question")
